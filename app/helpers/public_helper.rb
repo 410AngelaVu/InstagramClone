2e4f085a2bd5e8e1da888cbf819b4ce47dd76614
410AngelaVu <andjela1993@gmail.com>
2021-01-25 21:37:35 +0100

    Added style to the views public_nav

--- app/helpers/public_helper.rb
+++ app/helpers/public_helper.rb
@@ -1,2 +1,12 @@
 module PublicHelper
+
+def account_in
+if account_signed_in?
+	(link_to 'Log Out', destroy_account_session_path, method: :delete, class:'btn btn-secondary')
+else
+	(link_to 'Sign Up', new_account_registration_path, class:'btn btn-success') +
+	(link_to 'Log In', new_account_session_path, class:'btn btn-success')
 end
+end
+end
+
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,11 @@ class Post < ApplicationRecord
 belongs_to :account
 has_one_attached :image
 
+before_create :set_active
+
 scope :active, -> { where active: true }
+
+def set_active
+self.active = true
+	end
 end
