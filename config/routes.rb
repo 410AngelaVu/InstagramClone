b12c6ae228e66138733a1e12d9b4024853851ab8
410AngelaVu <andjela1993@gmail.com>
2021-01-26 01:12:48 +0100

    Added style for images

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :accounts
   get '/dashboard' => 'accounts#index'
+  get 'profile/:username' => 'accounts#profile', as: :profile
   resources :posts
   root to: "public#homepage"
 end
